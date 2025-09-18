7a19bf31edce250c2318f70655892922e990c16c
danlakelake <danlakelake@gmail.com>
2025-09-18 16:24:06 -0600

    se comenzo a trabajar con js y se elimino archivo styles

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const API_KEY = '196c7ac1af1a29e1dc7a86eeed186d23'; // <- API key OpenWeather
+const btn_search = document.getElementById('buscar');
+const result = document.getElementById('resultado');
 
+// Event Listener para botón buscar
+btn_search.addEventListener('click', () => {
+  const city = document.getElementById('ciudad').value;
+  console.log(city);
+  if (city === '') {
+    alert('Porfavor ingresa una ciudad');
+  }
+});
